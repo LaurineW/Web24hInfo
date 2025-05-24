5c44e609ffc88ba5af99bea007b932515d8eddbe
LapotreNicolas <nicolas62lapotre@gmail.com>
2025-05-24 04:16:58 +0200

    mise à jour des composants pour ajouter "use client";

--- src/components/Chapitre.tsx
+++ src/components/Chapitre.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {Chapitre} from "@/types/Chapitre";
 import React from "react";
 
--- src/components/Paragraphe.tsx
+++ src/components/Paragraphe.tsx
@@ -1,10 +1,13 @@
+"use client";
 import {Paragraphe} from "@/types/Paragraphe";
 import React from "react";
 
 export function Paragraphe(paragraphe : Paragraphe) : React.ReactElement {
     return <>
-            <h5>{paragraphe.nom}</h5>
-            <p>{paragraphe.texte}</p>
+            <div>
+                <h5>{paragraphe.nom}</h5>
+                <p>{paragraphe.texte}</p>
+            </div>
             <div>
                 {paragraphe.images.map(img => <img src={img} key={img} />)}
             </div>
