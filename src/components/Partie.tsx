b05d19fdade2faa6b346c055ff508869a172ee30
LapotreNicolas <nicolas62lapotre@gmail.com>
2025-05-24 02:53:06 +0200

    creation du composant pour les parties

---
+++ src/components/Partie.tsx
@@ -0,0 +1,12 @@
+import {Partie} from "@/types/Partie";
+import React from "react";
+
+export function Partie(partie : Partie) : React.ReactElement {
+    return <>
+            <h5>{partie.nom}</h5>
+            <p>{partie.resume}</p>
+            <div>
+                {partie.images.map(img => <img src={img} key={img}/>)}
+            </div>
+        </>
+}
